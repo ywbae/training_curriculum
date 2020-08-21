8f2c6a807274cf625d397ff7a184ac4c648ac6d6
YongWan Bae <yonwan.b@gmail.com>
2020-08-21 12:10:48 +0900

    Revert "Issue4"
    This reverts commit cb477490869fbd493e8d90cb92eb423a2f6e5cbd.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
